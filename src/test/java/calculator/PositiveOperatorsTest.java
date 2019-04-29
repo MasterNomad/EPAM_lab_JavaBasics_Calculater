f5e0de2656a0ca0249af39b40797bfe8636da358
Anton Sozonov <Unboundnomad@gmail.com>
2019-04-29 09:39:02 +0400

    fix test values

--- src/test/java/calculator/PositiveOperatorsTest.java
+++ src/test/java/calculator/PositiveOperatorsTest.java
@@ -12,45 +12,45 @@ public class PositiveOperatorsTest {
 
     @Test
     public void testAddition () {
-        BigDecimal a = BigDecimal.valueOf(1565124632);
-        BigDecimal b = BigDecimal.valueOf(561225462);
-        BigDecimal expResult = BigDecimal.valueOf(2126350094);
+        BigDecimal a = BigDecimal.valueOf(25);
+        BigDecimal b = BigDecimal.valueOf(89);
+        BigDecimal expResult = BigDecimal.valueOf(114);
         BigDecimal result = Operators.getEnum("+").apply(a, b);
         assertThat(result, equalTo(expResult));
     }
 
     @Test
     public void testSubtract () {
-        BigDecimal a = BigDecimal.valueOf(1565124632);
-        BigDecimal b = BigDecimal.valueOf(561225462);
-        BigDecimal expResult = BigDecimal.valueOf(1003899170);
+        BigDecimal a = BigDecimal.valueOf(83);
+        BigDecimal b = BigDecimal.valueOf(17);
+        BigDecimal expResult = BigDecimal.valueOf(66);
         BigDecimal result = Operators.getEnum("-").apply(a, b);
         assertThat(result, equalTo(expResult));
     }
 
     @Test
     public void testMultiply () {
-        BigDecimal a = BigDecimal.valueOf(1565124632);
-        BigDecimal b = BigDecimal.valueOf(561225462);
-        BigDecimal expResult = BigDecimal.valueOf(878387794681779984L);
+        BigDecimal a = BigDecimal.valueOf(12);
+        BigDecimal b = BigDecimal.valueOf(6);
+        BigDecimal expResult = BigDecimal.valueOf(72);
         BigDecimal result = Operators.getEnum("*").apply(a, b);
         assertThat(result, equalTo(expResult));
     }
 
     @Test
     public void testDivide () {
-        BigDecimal a = BigDecimal.valueOf(1565124632);
-        BigDecimal b = BigDecimal.valueOf(561225462);
-        BigDecimal expResult = BigDecimal.valueOf(2.7887626951608265).setScale(5, RoundingMode.HALF_UP);
+        BigDecimal a = BigDecimal.valueOf(81);
+        BigDecimal b = BigDecimal.valueOf(9);
+        BigDecimal expResult = BigDecimal.valueOf(9).setScale(5, RoundingMode.HALF_UP);
         BigDecimal result = Operators.getEnum("/").apply(a, b);
         assertThat(result, equalTo(expResult));
     }
 
     @Test
     public void testPow () {
-        BigDecimal a = BigDecimal.valueOf(15651);
-        BigDecimal b = BigDecimal.valueOf(65);
-        BigDecimal expResultGreater = new BigDecimal("4.418104091534241E+272");
+        BigDecimal a = BigDecimal.valueOf(5);
+        BigDecimal b = BigDecimal.valueOf(3);
+        BigDecimal expResultGreater = BigDecimal.valueOf(125.0);
         BigDecimal result = Operators.getEnum("^").apply(a, b);
         assertThat(result, equalTo(expResultGreater));
     }
